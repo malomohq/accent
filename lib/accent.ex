2bda8d9b5301b5d8c6a50d4c20569f1a37e1ff61
Anthony Smith <anthony@sticksnleaves.com>
2017-04-10 15:35:46 -0400

    Added support for camelizing string/atom

--- lib/accent.ex
+++ lib/accent.ex
@@ -1,18 +1,3 @@
 defmodule Accent do
-  @moduledoc """
-  Documentation for Accent.
-  """
-
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Accent.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
+  use Accent.CamelCase
 end
--- test/accent_test.exs
+++ test/accent_test.exs
@@ -1,8 +1,25 @@
 defmodule AccentTest do
   use ExUnit.Case
-  doctest Accent
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  describe "camelize/2" do
+    test "converts snake_case to camelCase by default" do
+      assert Accent.camelize("hello_world") == "helloWorld"
+    end
+
+    test "converts snake_case to camelCase when :lower" do
+      assert Accent.camelize("hello_world", :lower) == "helloWorld"
+    end
+
+    test "converts snake_case to CamelCase when :upper" do
+      assert Accent.camelize("hello_world", :upper) == "HelloWorld"
+    end
+
+    test "trims leading and trailing underscores from input" do
+      assert Accent.camelize("_hello_world_") == "helloWorld"
+    end
+
+    test "supports :atom as an input" do
+      assert Accent.camelize(:hello_world) == :helloWorld
+    end
   end
 end
