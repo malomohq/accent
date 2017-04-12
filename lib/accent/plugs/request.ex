510dcf50499810a61b25aa50a46ccd13d7bc22ec
Anthony Smith <anthony@sticksnleaves.com>
2017-04-12 11:49:29 -0400

    Added documentation for Accent.Plug.Request

--- lib/accent/plugs/request.ex
+++ lib/accent/plugs/request.ex
@@ -1,4 +1,34 @@
 defmodule Accent.Plug.Request do
+  @moduledoc """
+  Transform the keys of an HTTP request's params to the same or a different
+  case.
+
+  You can specify what case to convert keys to by passing in a `:transformer`
+  option.
+
+  Accent supports the following transformers out of the box:
+
+  * `Accent.Transformer.CamelCase` (e.g. `CamelCase`)
+  * `Accent.Transformer.PascalCase` (e.g. `pascalCase`)
+  * `Accent.Transformer.SnakeCase` (e.g. `snake_case`)
+
+  If no transformer is provided then `Accent.Transformer.SnakeCase` will be
+  used.
+
+  Please note that this plug will need to be executed after the request has
+  been parsed.
+
+  ### Example
+
+  ```
+  plug Plug.Parsers, parsers: [:urlencoded, :multipart, :json],
+                     pass: ["*/*"],
+                     json_decoder: Poison
+
+  plug Accent.Plug.Request, transformer: Accent.Transformer.CamelCase
+  ```
+  """
+
   @doc false
   def init(opts \\ []) do
     %{
